5d46108429a3d2866ff4859fa46ba0414c308051
soleecito <soledadrotiroti@gmail.com>
2020-10-19 22:12:29 -0300

    hashcode equals en deporte

--- src/ar/edu/unlam/pb2/smartWatch/Deporte.java
+++ src/ar/edu/unlam/pb2/smartWatch/Deporte.java
@@ -39,5 +39,38 @@ public abstract class Deporte {
 	public void aumentoDelTiempo() {
 		this.tiempoTranscurrido++;
 	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Deporte other = (Deporte) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 	
 }
